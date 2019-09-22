7ccbb627c6390935d30f5bbf7697024752e60a48
pogrebnyak <pogrebnyak.p@gmail.com>
2019-09-22 17:09:33 +0300

    1709

--- lesson1/hw_3.py
+++ lesson1/hw_3.py
@@ -1,4 +1,17 @@
 import math
 
-s=math.sqrt(367)
-print(s)
+while True:
+  print('Вычисление площади круга с радиусом R')
+  while True:
+    R = input('Введите радиус круга R (мм): ')
+    if R.isdigit() == 0 :
+        print('ОШИБКА! R должен быть целое цисло больше нуля')
+        continue
+    if int(R) == 0 :
+        print('ОШИБКА! R не может равняться нулю')
+        continue
+    break
+  S = round(math.pi*(int(R)**2),4)
+  print('Площадь круг c радиусом '+R+' мм = ',S,'мм' +u'\u00b2')
+  y = input('Вычислить площать прямоугольника еще раз (y/n)? ')
+  if y != 'y' : break
