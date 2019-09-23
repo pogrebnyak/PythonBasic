0b65545bdf003c2cafdb793849cc26ee7b83ba36
pogrebnyak <55633045+pogrebnyak@users.noreply.github.com>
2019-09-23 11:25:00 +0300

    Add files via upload

---
+++ lessons01/hw_3.py
@@ -0,0 +1,17 @@
+import math
+
+while True:
+  print('Вычисление площади круга с радиусом R')
+  while True:
+    R = input('Введите радиус круга R (мм): ')
+    if R.isdigit() == 0:
+        print('ОШИБКА! R должен быть целое цисло больше нуля')
+        continue
+    if int(R) == 0:
+        print('ОШИБКА! R не может равняться нулю')
+        continue
+    break
+  S = round(math.pi*(int(R)**2), 4)
+  print('Площадь круга c радиусом '+R+' мм = ', S, 'мм' +u'\u00b2')
+  y = input('Вычислить площадь круга еще раз (y/n)? ')
+  if y != 'y': break
