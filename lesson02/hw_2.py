8ec2948eb9da77c19eb8fb10b8eccc50290f322e
pogrebnyak <pogrebnyak.p@gmail.com>
2019-09-26 22:34:15 +0300

    Final

--- lesson02/hw_2.py
+++ lesson02/hw_2.py
@@ -1,7 +1,7 @@
+import math
+
 print("""Пользователь вводит два любых числа.
 Выдать список всех натуральных чисел между этими двумя числами.""")
-
-import math
 
 a = float(input('a: '))
 b = float(input('b: '))
@@ -11,6 +11,6 @@ if a > b:
 a = int(a)
 b = math.ceil(b)  # округляет в большую сторону. (int)
 
-c = [i for i in range(a+1,b) if i > 0]
+c = [i for i in range(a + 1, b) if i > 0]
 
 print(c)
