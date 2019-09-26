e9c686137be41cd5a1f79a203499c57751043f61
pogrebnyak <pogrebnyak.p@gmail.com>
2019-09-26 22:21:47 +0300

    Final

--- lesson02/hw_2.py
+++ lesson02/hw_2.py
@@ -5,11 +5,12 @@ import math
 
 a = float(input('a: '))
 b = float(input('b: '))
-a = math.ceil(a)  #округляет в большую сторону. (int)
-b = math.ceil(b)  #округляет в большую сторону. (int)
 if a > b:
     a, b = b, a
-       
-c = [i for i in range(a,b) if i > 0]   
+
+a = int(a)
+b = math.ceil(b)  # округляет в большую сторону. (int)
+
+c = [i for i in range(a+1,b) if i > 0]
 
 print(c)
