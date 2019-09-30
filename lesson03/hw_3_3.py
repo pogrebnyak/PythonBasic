c4781033328b6f94e459e8f9b7dddb039a81001d
pogrebnyak <pogrebnyak.p@gmail.com>
2019-09-30 23:29:02 +0300

    Final

---
+++ lesson03/hw_3_3.py
@@ -0,0 +1,20 @@
+while True:
+    print("1. Площадь круга")
+    print("2. Площадь треугольника")
+    print("3. Площадь прямоугольника")
+    print("4. Выход")
+    y = input('Выберите (1,2,3,4): ')
+    if y == '1':
+        print('1')
+        break
+    elif y == '2':
+        print('2')
+        break
+    elif y == '3':
+        print('3')
+        break
+    elif y == '4':
+        print('4')
+        break
+    else:
+        print('5')
