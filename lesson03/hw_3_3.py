c4843d7b7885549c1e88ac472b4c884f683b815c
pogrebnyak <55633045+pogrebnyak@users.noreply.github.com>
2019-10-01 13:06:43 +0300

    Beta v1.01

--- lesson03/hw_3_3.py
+++ lesson03/hw_3_3.py
@@ -1,20 +1,72 @@
+import math
+
+menu_choice =('1','2','3','4')
+
+def input_value(str_value):
+    while True:
+        try:
+            f = float(input(str_value + ': '))
+            if f < 0:
+                print('Должно быть больше нуля'+'\n')
+                continue
+            break
+        except ValueError:
+            print('ОШИБКА! Вы должны ввести целое или вещественное число'+'\n')
+    return f
+
+def circle(r):
+  area_circle = math.pi*(float(r)**2)
+  return area_circle
+
+def rect(a,b):
+  area = a * b
+  perimeter = 2 * (a + b)
+  return area, perimeter 
+
+def triangle(a,b,c):
+    p = (a + b + c) / 2
+    podkoren = p * (p - a) * (p - b) * (p - c)
+    if podkoren < 0 or podkoren == 0:
+        print('Такого треугольник не существует'+'\n')
+        area = None
+    else:
+        area = math.sqrt(podkoren)
+        return area
+  
 while True:
-    print("1. Площадь круга")
-    print("2. Площадь треугольника")
-    print("3. Площадь прямоугольника")
+    print("1. Расчитать площадь круга")
+    print("2. Расчитать площадь треугольника по формуле Герона")
+    print("3. Расчитать площадь и периметр прямоугольника")
     print("4. Выход")
-    y = input('Выберите (1,2,3,4): ')
+    
+    while True:
+        y = input('Выберите (1,2,3,4): ')
+        if y not in menu_choice:
+          print('ОШИБКА! Вы дожны выбрать из (1,2,3,4)\n')
+          continue
+        break
+    
     if y == '1':
-        print('1')
-        break
+        print(' ')
+        radius_circle = input_value('Введите радиус круга')
+        print('Площадь круга: ' + str(circle(radius_circle))+'\n')
     elif y == '2':
-        print('2')
-        break
+        print(' ')
+        while True:
+          triangle_a = input_value('Введите сторону a')
+          triangle_b = input_value('Введите сторону b')
+          triangle_c = input_value('Введите сторону c')
+          area_triangle = triangle(triangle_a,triangle_b,triangle_c)
+          if area_triangle != None:
+              print('Площадь треугольника: ' + str(area_triangle)+'\n')
+              break
     elif y == '3':
-        print('3')
-        break
+        print(' ')
+        rect_a = input_value('Введите длину прямоугольника')
+        rect_b = input_value('Введите ширину прямоугольника')
+        area_rect, perimeter_rect = rect(rect_a,rect_b)
+        print('Площадь прямоугольника: ' + str(area_rect))
+        print('Периметр прямоугольника: ' + str(perimeter_rect)+'\n')
     elif y == '4':
         print('Выход')
         break
-    else:
-        print('5')
