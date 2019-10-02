d7d559cb6887148752e42d4cdc3f2b5c7ea98bba
pogrebnyak <55633045+pogrebnyak@users.noreply.github.com>
2019-10-02 10:12:10 +0300

    final

--- lesson03/hw_3_1.py
+++ lesson03/hw_3_1.py
@@ -1,22 +1,24 @@
-y = 'y'
+def calc(a):
+  res = 0
+  for i in str(a):
+    if i.isdigit():
+      res += 1
+  return res
 
-print('Программа подсчета цифр в числе')
 
-while y == 'y':
-  def calc(a):
-    res = 0
-    for i in str(a):
-        if i.isdigit():
-          res += 1
-    return res
+if __name__ == "__main__":
 
-  while True:
-    global a
-    try:
-      input_value = int(input('Введите цело число: '))
-      break
-    except ValueError:
-      print('ОШИБКА! Вы должны ввести целое число')
+  print('Программа подсчета цифр в числе')
+  y = 'y'
+  while y == 'y':
+    
+    while True:
+      global a
+      try:
+        input_value = int(input('Введите цело число: '))
+        break
+      except ValueError:
+        print('ОШИБКА! Вы должны ввести целое число')
 
-  print('В числе ' + str(input_value) + ' цифр : ' + str(calc(input_value)))
-  y = input('Еще раз? (y/n): ')
+    print('В числе ' + str(input_value) + ' цифр : ' + str(calc(input_value)))
+    y = input('Еще раз? (y/n): ')
