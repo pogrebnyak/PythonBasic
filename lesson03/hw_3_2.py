7327e82fad63325b351b00c35ec19fc3b097bd6c
pogrebnyak <55633045+pogrebnyak@users.noreply.github.com>
2019-10-02 10:20:13 +0300

    final

--- lesson03/hw_3_2.py
+++ lesson03/hw_3_2.py
@@ -10,7 +10,7 @@ def input_value(str_value):
     while True:
         try:
             f = int(input(str_value + ': '))
-            if f < 0:
+            if f <= 0:
                 print('Должно быть больше нуля')
                 continue
             break
@@ -18,13 +18,15 @@ def input_value(str_value):
             print('ОШИБКА! Вы должны ввести целое число')
     return f
 
-print('Программа выводит НОК двух чисел a и b')
+if __name__ == "__main__":
+    
+    print('Программа выводит НОК двух чисел a и b')
 
-y = 'y'
+    y = 'y'
 
-while y == 'y':
-  input_a = input_value('Введите a')
-  input_b = input_value('Введите b')
-  
-  print('НОК чисел ' + str(input_a) + ' и ' + str(input_b) + ': ' + str(nok(input_a, input_b)))
-  y = input('Еще раз? (y/n): ')
+    while y == 'y':
+      input_a = input_value('Введите a')
+      input_b = input_value('Введите b')
+      
+      print('НОК чисел ' + str(input_a) + ' и ' + str(input_b) + ': ' + str(nok(input_a, input_b)))
+      y = input('Еще раз? (y/n): ')
