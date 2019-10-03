1eb6eca6c129e637b27bf320685478ba73586285
pogrebnyak <55633045+pogrebnyak@users.noreply.github.com>
2019-10-03 16:15:27 +0300

    alfa

---
+++ lesson04/hw_4_2.py
@@ -0,0 +1,41 @@
+def text_stat(text):
+  text_dict = {}
+  for i in text:
+     if text_dict.get(i) is not None:
+         continue
+     else:
+         text_dict[i] = text.count(i)
+
+  return text_dict
+
+def digit_count(text):
+  s = 0
+  for i in text:
+      if i.isdigit():
+          s += 1
+  return s
+
+text_input = input('В ведите строку : ')  
+
+text_input = text_input.lower()
+text_list = text_input.split()
+lines = len(text_input.splitlines())
+digits = digit_count(text_input)
+words_stat = text_stat(text_list)
+chars_stat = text_stat(text_input)
+print('')
+print('Всего строк: '+str(lines)+'\n')
+
+print('Всего цифр: '+str(digits)+'\n')
+
+word_sorted = list(words_stat.keys())
+word_sorted.sort()
+  
+for i in word_sorted:
+    print(repr(i),'=',words_stat[i])
+print('')
+chars_sorted = list(chars_stat.keys())
+chars_sorted.sort()
+  
+for i in chars_sorted:
+    print(repr(i),'=',chars_stat[i])
