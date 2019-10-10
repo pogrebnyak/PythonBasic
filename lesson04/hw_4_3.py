8a3e0b18bb637e3c54c3a02689876d743b85e7b7
pogrebnyak <pogrebnyak.p@gmail.com>
2019-10-10 20:33:54 +0300

    alfa

--- lesson04/hw_4_3.py
+++ lesson04/hw_4_3.py
@@ -1,12 +1,10 @@
-def text_stat(text,filter = ''):
+def text_stat(text,*filter):
     text_dict = {}
     for i in text:
         if text_dict.get(i) is not None:
             continue
         else:
             text_dict[i] = text.count(i)
-            
-    filter = filter.lower().split()
 
     for i in filter:
         if text_dict.get(i) is None:
@@ -20,20 +18,20 @@ while True:
     if text_input == '':
         break
     exception_input = input('Введите исключения через пробел: ')
+    exception_input = exception_input.lower().split()
 
 
     # text_input = '''About 90 percent of all children attend public school, which is free. The other 10 percent go I private schools,
     # which often include religious education. They are similar to the public schools but parents must pay for their children to go to
     # these schools. About half of all private schools are run by Catholics.'''
 
-    # exception_input = ''
-    # exception_input = 'of'
-    # exception_input = 'of all'
-    # exception_input = 'of all two'
-
     text_input = text_input.lower()
     text_list = text_input.split()
-    words_stat = text_stat(text_list,exception_input)
+    words_stat = text_stat(text_list,*exception_input)
+    # words_stat = text_stat(text_list)
+    # words_stat = text_stat(text_list,'of')
+    # words_stat = text_stat(text_list,'of','all')
+    # words_stat = text_stat(text_list,'of','all','two')
     print('')
 
     word_sorted = list(words_stat.keys())
